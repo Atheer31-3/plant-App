d2677050ef99c1d5d23ff2ab2d1b8e47fe3647a8
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2024-10-17 18:50:16 +0300

    add pages

---
+++ plant/plant/Delete EDIT Plant.swift
@@ -0,0 +1,18 @@
+//
+//  Delete EDIT Plant.swift
+//  plant
+//
+//  Created by atheer alshareef on 17/10/2024.
+//
+
+import SwiftUI
+
+struct Delete_EDIT_Plant: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Delete_EDIT_Plant()
+}
---
+++ plant/plant/my Plants.swift
@@ -0,0 +1,18 @@
+//
+//  Today Reminder.swift
+//  plant
+//
+//  Created by atheer alshareef on 17/10/2024.
+// ** my main page **
+
+import SwiftUI
+
+struct my_Plants: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    my_Plants()
+}
