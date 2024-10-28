5fb81b08d2c7fdb0a1c973e64673679e9facfec3
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2024-10-28 20:46:44 +0300

    Update SetReminderView.swift
    new SetReminderView within swiftData

--- plant/plant/SetReminderView.swift
+++ plant/plant/SetReminderView.swift
@@ -1,98 +1,61 @@
-////
-////  Set Reminder.swift
-////  plant
-////
-////  Created by atheer alshareef on 17/10/2024.
 //
+//  Set Reminder.swift
+//  plant
+//
+//  Created by atheer alshareef on 17/10/2024.
 
 import SwiftUI
+import SwiftData
 
 struct SetReminderView: View {
-    @Binding var plants: [Plant]
-    @State private var name = ""
-    @State private var room = "Bedroom"
-   // @State private var sunlight = "Full Sun"
-    @State private var sunlight: SunlightType = .fullSun
-    @State private var wateringDays = "Every day"
-    @State private var waterAmount = "20-50 ml"
-    @Environment(\.presentationMode) var presentationMode
-    var plantToEdit: Plant?
-
+    @ObservedObject var viewModel: PlantViewModel
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.presentationMode) private var presentationMode
+        
     var body: some View {
         NavigationView {
             Form {
                 Section {
                     HStack {
-                        Text("Plant Name")
-                            .foregroundColor(.white)
-                        TextField("Pothos", text: $name)
-                            .foregroundColor(.white)
-                            .tint(.c1)
+                         Text("Plant Name")
+                         .foregroundColor(.white)
+                        TextField("Pothos", text: $viewModel.name)
+                         .foregroundColor(.white)
+                         .tint(.c1)
                     }
                 }
-                
                 Section {
                     HStack{
-                        Image(systemName: "location")
-                            .foregroundColor(.white)
-                        Picker("Room", selection: $room) {
-                            Text("Bedroom").tag("Bedroom")
-                            Text("Living Room").tag("Living Room")
-                            Text("Kitchen").tag("Kitchen")
-                            Text("Balcony").tag("Balcony")
-                            Text("Bathroom").tag("Bathroom")
-                        }
+                    Image(systemName: "location")
+                    .foregroundColor(.white)
+                        Picker("Room", selection: $viewModel.room) {
+                         Text("Bedroom").tag("Bedroom")
+                         Text("Living Room").tag("Living Room")
+                         Text("Kitchen").tag("Kitchen")
+                         Text("Balcony").tag("Balcony")
+                         Text("Bathroom").tag("Bathroom")
+                       }
                     }
-                    
                     HStack {
-                        Image(systemName: sunlight.icon())
-                            .foregroundColor(.white)
-                        
-                        Picker("Light", selection: $sunlight) {
+                        Image(systemName: "sun.max")
+                        .foregroundColor(.white)
+                        Picker("Light", selection: $viewModel.sunlight) {
                             ForEach(SunlightType.allCases, id: \.self) { type in
                                 HStack {
-                                    Text(type.rawValue)
                                     Image(systemName: type.icon())
-                                }.tag(type)
+                                    Text(type.displayName())
+                                }
+                                .tag(type.rawValue)
                             }
                         }
                     }
+
                 }
-                
-                
-                //                    HStack {
-                //                        Image(systemName: "sun.max")
-                //                            .foregroundColor(.white)
-                //                        Picker("Light", selection: $sunlight) {
-                //                            HStack {
-                //                                Text("Full Sun")
-                //                                Image(systemName: "sun.max")
-                //
-                //                            }
-                //                            .tag("Full Sun")
-                //
-                //                            HStack {
-                //                                Text("Partial Sun")
-                //                                Image(systemName: "sun.haze")
-                //
-                //                            }
-                //                            .tag("Partial Sun")
-                //
-                //                            HStack {
-                //                                Text("Low Light")
-                //                                Image(systemName: "moon")
-                //
-                //                            }
-                //                            .tag("Low Light")
-                //                        }
-                //                    }
-                
-                
                 Section {
-                    HStack {
+                    HStack{
                         Image(systemName: "drop")
                             .foregroundColor(.white)
-                        Picker("Watering Days", selection: $wateringDays) {
+                        Picker("Watering Days", selection: $viewModel.wateringDays) {
                             Text("Every day").tag("Every day")
                             Text("Every 2 days").tag("Every 2 days")
                             Text("Every 3 days").tag("Every 3 days")
@@ -101,13 +64,10 @@ struct SetReminderView: View {
                             Text("Every 2 weeks").tag("Every 2 weeks")
                         }
                     }
-                    
-                    //   .padding(.vertical, 6)
-                    
-                    HStack {
+                    HStack{
                         Image(systemName: "drop")
                             .foregroundColor(.white)
-                        Picker("Water Amount", selection: $waterAmount) {
+                        Picker("Water", selection: $viewModel.waterAmount) {
                             Text("20-50 ml").tag("20-50 ml")
                             Text("50-100 ml").tag("50-100 ml")
                             Text("100-200 ml").tag("100-200 ml")
@@ -116,61 +76,81 @@ struct SetReminderView: View {
                     }
                 }
                 
-                if plantToEdit != nil {
+                if viewModel.selectedPlant != nil {
                     Section {
-                        Button(action: deletePlant) {
+                        Button(action: {
+                            if let plant = viewModel.selectedPlant {
+                                modelContext.delete(plant)
+                            }
+                            viewModel.clearSelectedPlant()
+                            presentationMode.wrappedValue.dismiss()
+                        }) {
                             Text("Delete Reminder")
                                 .foregroundColor(.c2)
+                                .font(.system(size: 20))
                                 .frame(maxWidth: .infinity)
                                 .multilineTextAlignment(.center)
-                                .font(.system(size: 20))
-                                .padding(.vertical, 8)
+                                .padding(.vertical, 6)
                         }
+                        .buttonStyle(PlainButtonStyle())
                     }
                 }
-                
             }
-            .navigationBarTitle(plantToEdit == nil ? "Set Reminder" : "Edit Reminder", displayMode: .inline)
             
+            .navigationTitle(viewModel.selectedPlant == nil ? "Add Reminder" : "Edit Reminder")
+            .navigationBarTitleDisplayMode(.inline)
             .navigationBarItems(
                 leading: Button("Cancel") {
                     presentationMode.wrappedValue.dismiss()
                 }
-                    .foregroundColor(Color.c1),
-                
+                    .foregroundColor(.c1),
                 trailing: Button("Save") {
                     savePlant()
+                    presentationMode.wrappedValue.dismiss()
                 }
-                    .foregroundColor(Color.c1)
+                    .foregroundColor(.c1)
             )
             .onAppear {
-                if let plant = plantToEdit {
-                    name = plant.name
-                    room = plant.room
-                    sunlight = plant.sunlight
-                    wateringDays = plant.wateringDays
-                    waterAmount = plant.waterAmount
+                if let plant = viewModel.selectedPlant {
+                    viewModel.name = plant.name
+                    viewModel.room = plant.room
+                    viewModel.sunlight = viewModel.sunlight
+                    viewModel.wateringDays = plant.wateringDays
+                    viewModel.waterAmount = plant.waterAmount
                 }
             }
         }
     }
     
-    private func savePlant() {
-        if let plant = plantToEdit {
-            if let index = plants.firstIndex(where: { $0.id == plant.id }) {
-                plants[index] = Plant(id: plant.id, name: name, room: room, sunlight: sunlight, wateringDays: wateringDays, waterAmount: waterAmount)
-            }
-        } else {
-            let newPlant = Plant(name: name, room: room, sunlight: sunlight, wateringDays: wateringDays, waterAmount: waterAmount)
-            plants.append(newPlant)
+     func deletePlant() {
+        if let plant = viewModel.plantToEdit {
+            modelContext.delete(plant)
         }
         presentationMode.wrappedValue.dismiss()
     }
 
-    private func deletePlant() {
-        if let plant = plantToEdit, let index = plants.firstIndex(where: { $0.id == plant.id }) {
-            plants.remove(at: index)
+     func savePlant() {
+        if let plant = viewModel.selectedPlant {
+            plant.name = viewModel.name
+            plant.room = viewModel.room
+            plant.sunlight = viewModel.sunlight
+            plant.wateringDays = viewModel.wateringDays
+            plant.waterAmount = viewModel.waterAmount
+        } else {
+            _ = SunlightType(rawValue: viewModel.sunlight)?.rawValue ?? SunlightType.fullSun.rawValue
+            let newPlant = Plant(
+                name: viewModel.name,
+                room: viewModel.room,
+                sunlight: SunlightType(rawValue: viewModel.sunlight) ?? .fullSun,
+                wateringDays: viewModel.wateringDays,
+                waterAmount: viewModel.waterAmount
+            )
+            modelContext.insert(newPlant)
         }
-        presentationMode.wrappedValue.dismiss()
+        viewModel.clearSelectedPlant()
+        viewModel.name = ""
+        viewModel.room = ""
+        
     }
 }
+
