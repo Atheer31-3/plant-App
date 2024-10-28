ffebbbb000dd20970f22afb485df26e1d6fa8372
Atheer Alshareef <105993810+Atheer31-3@users.noreply.github.com>
2024-10-28 20:45:52 +0300

    Update Plant.swift
    new plant within swiftData

--- plant/plant/Plant.swift
+++ plant/plant/Plant.swift
@@ -3,43 +3,54 @@
 //  plant
 //
 //  Created by atheer alshareef on 17/10/2024.
-// data for plantas
 
-import SwiftUI
-struct Plant: Identifiable {
-    var id = UUID()
+import SwiftData
+
+@Model
+class Plant {
     var name: String
     var room: String
-    var sunlight: SunlightType
+    var sunlight: Int
     var wateringDays: String
     var waterAmount: String
-    var isWatered: Bool = false
-
-    // Custom initializer
-    init(id: UUID = UUID(), name: String, room: String, sunlight: SunlightType, wateringDays: String, waterAmount: String, isWatered: Bool = false) {
-        self.id = id
+    var isWatered: Bool
+    
+    init(name: String, room: String, sunlight: SunlightType, wateringDays: String, waterAmount: String, isWatered: Bool = false) {
         self.name = name
         self.room = room
-        self.sunlight = sunlight
+        self.sunlight = sunlight.rawValue
         self.wateringDays = wateringDays
         self.waterAmount = waterAmount
         self.isWatered = isWatered
     }
+    var sunlightType: SunlightType? {
+           get { SunlightType(rawValue: sunlight) }
+           set { sunlight = newValue?.rawValue ?? SunlightType.fullSun.rawValue }
+       }
 }
 
-enum SunlightType: String, CaseIterable {
-    case fullSun = "Full Sun"
-    case partialSun = "Partial Sun"
-    case lowLight = "Low Light"
-    
+
+enum SunlightType: Int, CaseIterable {
+    case fullSun = 0
+    case partialSun = 1
+    case lowLight = 2
+
     func icon() -> String {
         switch self {
-        case .fullSun:
-            return "sun.max"
-        case .partialSun:
-            return "sun.haze"
-        case .lowLight:
-            return "moon"
+        case .fullSun: return "sun.max"
+        case .partialSun: return "sun.haze"
+        case .lowLight: return "moon"
+        }
+    }
+
+    func displayName() -> String {
+        switch self {
+        case .fullSun: return "Full Sun"
+        case .partialSun: return "Partial Sun"
+        case .lowLight: return "Low Light"
         }
     }
 }
+
+
+
